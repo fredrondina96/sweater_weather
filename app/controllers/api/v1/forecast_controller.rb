d3304c49e54e078ddce0e701871e0038b2801b11
Fred Rondina <fredrondina96@gmail.com>
2020-06-06 16:06:22 -0600

    added ability to get lat and long from google api

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def show
+      response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?') do |req|
+        req.params['address'] = params[:location]
+        req.params['key'] = ENV['GOOGLE_API_KEY']
+      end
+    coordinates = JSON.parse(response.body, symbolize_names: true)[:results].first[:geometry][:location]
     binding.pry
   end
 end
