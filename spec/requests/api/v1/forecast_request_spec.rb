c41cc0e1509e0eed8c200a1840a2287e3ffef9bb
Fred Rondina <fredrondina96@gmail.com>
2020-06-06 15:08:05 -0600

    added skeleton of first test

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Requets', type: :request do
+  it 'should return the forcast for a given city' do
+
+    get '/api/v1/forecast?location=denver,co'
+    expect(response).to be_successful
+
+  end
+end
