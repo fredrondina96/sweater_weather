079bddf6e9ca8834e16b2a04c231a7b8639349a0
Fred Rondina <fredrondina96@gmail.com>
2020-06-07 16:02:40 -0600

    finished rough show method for forecast controller, needs facade refctor

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def show
     location_data = Location.new(params[:location])
-    binding.pry
+    weather_data = OpenWeatherService.new.weather_data(location_data.lat, location_data.long)
+    forecast = Forecast.new(params[:location], weather_data)
+    render json: ForecastSerializer.new(forecast)
   end
 end
