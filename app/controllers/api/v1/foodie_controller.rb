6164a4d6f39cbd9704c2cb7f30e40d3852259be6
Fred Rondina <fredrondina96@gmail.com>
2020-06-08 11:24:24 -0600

    removed redundant method call

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::FoodieController < ApplicationController
 
   def show
-    travel_time = TravelTime.new(params[:start], params[:end])
+    travel_time = TravelTime.new(params[:start], params[:end]).time
     restaurant = Restaurant.new(Location.new(params[:end]), params[:search])
     forecast = ForecastFacade.new(params[:end]).quick_forecast
-    travel_food = TravelFood.new(params[:end], travel_time.time, forecast, restaurant)
+    travel_food = TravelFood.new(params[:end], travel_time, forecast, restaurant)
     render json: TravelFoodSerializer.new(travel_food)
   end
 end
