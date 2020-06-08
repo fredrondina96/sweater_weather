1a5af0c16e004611c6e15ee6247991e702426015
Fred Rondina <fredrondina96@gmail.com>
2020-06-08 10:47:37 -0600

    Refactored restaurant poro

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -12,27 +12,8 @@ class Api::V1::FoodieController < ApplicationController
 
     coordinates = Location.new(params[:end])
 
-    # cusine id
-    response = Faraday.get('https://developers.zomato.com/api/v2.1/cuisines?') do |req|
-      req.params['lat'] = coordinates.lat
-      req.params['lon'] = coordinates.long
-      req.headers['user-key'] = ENV.fetch('ZOMATO_API_KEY')
-    end
-    food_ids = JSON.parse(response.body, symbolize_names: true)[:cuisines]
-
-      food_id = food_ids.find do |food|
-         food[:cuisine][:cuisine_name] == params[:search].capitalize
-      end
-
-    #get restauraunt
-    response = Faraday.get('https://developers.zomato.com/api/v2.1/search') do |req|
-      req.params['lat'] = coordinates.lat
-      req.params['lon'] = coordinates.long
-      req.params['cuisines'] = food_id[:cuisine][:cuisine_id]
-      req.headers['user-key'] = ENV.fetch('ZOMATO_API_KEY')
-    end
-    restaurant = JSON.parse(response.body, symbolize_names: true)[:restaurants].first
-    # binding.pry
+    restaurant = Restaurant.new(coordinates, params[:search])
+    
     forecast = ForecastFacade.new(params[:end])
     weather_summary = ("Temp is #{forecast.data.current_temp}, Forecast is #{forecast.data.current_description}")
     travel_food = TravelFood.new(params[:end], travel_time, weather_summary, restaurant)
--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -2,21 +2,35 @@ class Restaurant
   attr_reader :name,
               :address
 
-  def initialize(location)
-    @data = load_data
-    @name = @location[:lat]
-    @address = @location[:lng]
+  def initialize(coordinates, cuisine)
+    @data = load_data(coordinates, cuisine)
+    @name = @data[:restaurant][:name]
+    @address = @data[:restaurant][:location][:address]
   end
 
   private
 
-  def load_location(location)
-    GoogleService.new.load_location_data(location)
-    # binding.pry
-    # response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?') do |req|
-    #   req.params['address'] = location
-    #   req.params['key'] = ENV['GOOGLE_API_KEY']
-    # end
-    # coordinates = JSON.parse(response.body, symbolize_names: true)[:results].first[:geometry][:location]
+  def load_data(coordinates, cuisine)
+    # cusine id
+    response = Faraday.get('https://developers.zomato.com/api/v2.1/cuisines?') do |req|
+      req.params['lat'] = coordinates.lat
+      req.params['lon'] = coordinates.long
+      req.headers['user-key'] = ENV.fetch('ZOMATO_API_KEY')
+    end
+    food_ids = JSON.parse(response.body, symbolize_names: true)[:cuisines]
+
+      food_id = food_ids.find do |food|
+         food[:cuisine][:cuisine_name] == cuisine.capitalize
+      end
+
+    #get restauraunt
+    response = Faraday.get('https://developers.zomato.com/api/v2.1/search') do |req|
+      req.params['lat'] = coordinates.lat
+      req.params['lon'] = coordinates.long
+      req.params['cuisines'] = food_id[:cuisine][:cuisine_id]
+      req.headers['user-key'] = ENV.fetch('ZOMATO_API_KEY')
+    end
+    restaurant = JSON.parse(response.body, symbolize_names: true)[:restaurants].first
+  
   end
 end
