e7700036f3eca5cd2b01082979572cb985e4e37c
Fred Rondina <fredrondina96@gmail.com>
2020-06-08 18:59:19 -0600

    refactored background controller, now properly encasulated

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-    response = Faraday.get 'https://api.unsplash.com/search/photos?' do |req|
-      req.params['query'] = params[:location]
-      req.params['per_page'] = 1
-      req.params['count'] = 1
-      req.params['client_id'] = ENV['UNSPLASH_API_KEY']
-    end
-   image_url = JSON.parse(response.body, symbolize_names: true)[:results].first[:urls][:full]
-   binding.pry
+   image = Background.new(params[:location])
+   render json: BackgroundSerializer.new(image)
  end
 end
