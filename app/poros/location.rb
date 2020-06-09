462e65ceade666bf39d4de07fe7165eaea3b45eb
Fred Rondina <fredrondina96@gmail.com>
2020-06-09 17:44:20 -0600

    Removed pry and old comments

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -12,11 +12,5 @@ class Location
 
   def load_location(location)
     GoogleService.new.load_location_data(location)
-    # binding.pry
-    # response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?') do |req|
-    #   req.params['address'] = location
-    #   req.params['key'] = ENV['GOOGLE_API_KEY']
-    # end
-    # coordinates = JSON.parse(response.body, symbolize_names: true)[:results].first[:geometry][:location]
   end
 end
